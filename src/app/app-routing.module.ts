9736c7d3668292490840b74e3b991ea893158339
gordian05 <arragon05@gmail.com>
2019-06-11 21:39:02 +0200

    mise en place detail-client

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListClientComponent } from './features/client/list-client/list-client.component';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { DetailClientComponent } from './features/client/detail-client/detail-client.component';
 
 const routes: Routes = [
   { path: '', component: ListClientComponent },
   { path: 'home', component: DashboardComponent },
+  { path: 'detailClient', component: DetailClientComponent },
 ];
 
 @NgModule({
